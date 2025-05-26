5a0e63cf03e35add1f902122ca49ba751442a683
Farrel <farrelvaleryu7@gmail.com>
2025-05-27 06:43:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("Selamat datang web cara mengundurkan diri dari osn")
-st.write("by Farrel")
+st.write("by Farrel Fawwaz")
 st.image("IMG_4589.png")
 st.image("IMG_4588.png")
