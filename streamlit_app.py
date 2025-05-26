9919910c52e7f685785de0cf5b603778323a35b5
Farrel <farrelvaleryu7@gmail.com>
2025-05-27 06:32:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Infor mati, fisika masak, cuakss")
+st.write("Farrel/zerph/Fawwaz/Ahli Hukum")."
 )
